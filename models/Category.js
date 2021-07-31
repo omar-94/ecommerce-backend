d93ea9b6cc7feb52fdb008a4f0ed0afc608c1ee9
onava <enava23@yahoo.com>
2021-07-31 02:09:06 -0400

    product model created

--- models/Category.js
+++ models/Category.js
@@ -11,11 +11,11 @@ Category.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
     category_name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     }
   },
   {
