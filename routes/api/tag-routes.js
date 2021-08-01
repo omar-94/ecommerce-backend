369478c1dd8629aa92cf0de401eaec3e470c299d
onava <enava23@yahoo.com>
2021-07-31 23:35:48 -0400

    find tag by id route added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -27,6 +27,32 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+        through: ProductTag,
+        as: "products",
+      },
+    ],
+  })
+  .then((dbTagData) => {
+    if (!dbTagData) {
+      res
+        .status(404)
+        .json({ message: "There was no tag found with this id."});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
