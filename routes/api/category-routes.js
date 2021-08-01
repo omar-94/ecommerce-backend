ee17be0648d56893cb01f212f324d1ec5435dcdd
onava <enava23@yahoo.com>
2021-07-31 22:13:30 -0400

    find category by id route added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,6 +25,30 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+  .then((dbCategoryData) => {
+    if (!dbCategoryData) { 
+      res
+        .status(404)
+        .json({ message: "There was no category found for this id."});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
