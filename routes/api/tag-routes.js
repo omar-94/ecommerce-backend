00c96cd4a3ee1a935af84c7ee64b88b70ad6b5b8
onava <enava23@yahoo.com>
2021-07-31 23:44:53 -0400

    delete tag by id route added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -91,6 +91,24 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((dbTagData) => {
+    if (!dbTagData) {
+      res
+        .status(404)
+        .json({ message: "There was no tag found with this id."});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
