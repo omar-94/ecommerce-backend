2c0102536421b7d2965c2538cb707b90e977278f
onava <enava23@yahoo.com>
2021-07-31 22:31:06 -0400

    update category by id route added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -65,6 +65,29 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+  .then((dbCategoryData) => {
+    if (!dbCategoryData) {
+      res
+        .status(404)
+        .json({ message: "There was no category found with this id."});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
