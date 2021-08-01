beecc8afdd6d79321b4162f6554a8b410bc97845
onava <enava23@yahoo.com>
2021-07-31 22:23:38 -0400

    create new category route added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -53,6 +53,14 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+  .then((dbProductData) => res.json(dbProductData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
