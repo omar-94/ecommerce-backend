5177d9428c6848b6a4241706226bbb863bbb9966
onava <enava23@yahoo.com>
2021-07-31 21:11:48 -0400

    find all categories route added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,20 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+  .then((dbCategoryData) => res.json(dbCategoryData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
